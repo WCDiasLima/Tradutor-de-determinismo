c79756f534cd23235ea824a9ffdc145d7a880366
Wellington Cesar <wcdiaslima@outlook.com.br>
2019-04-16 01:28:41 -0300

    Formatação

--- Main.js
+++ Main.js
@@ -1,29 +1,28 @@
 /*
-Copyright 2019 Joyce Emanuele, Wellington Cesar
+ Copyright 2019 Joyce Emanuele, Wellington Cesar
 
-This file is part of Tradutor de Determinismo (TdD).
+ This file is part of Tradutor de Determinismo (TdD).
 
-TdD is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+ TdD is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-TdD is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ TdD is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with TdD. If not, see <https://www.gnu.org/licenses/>.
-*/
+ You should have received a copy of the GNU General Public License
+ along with TdD. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 let entrada, saida, botao;
 let AFN, AFD;
 
 function setup() {
-	
-       entrada = document.getElementById('nde');
-       saida = document.getElementById('det');
+	entrada = document.getElementById('nde');
+	saida = document.getElementById('det');
 
 	AFD = {
 		"alfabeto": undefined,
@@ -39,9 +38,11 @@ function traduzir() {
 	AFD.alfabeto = AFN.alfabeto;
 	AFD.estadosFinais = [];
 	AFD.delta = [];
+
 	let estados = [];
 	let falta = [];
 	let temp = [];
+
 	falta.push(new Set(AFN.estadoInicial));
 	while(falta.length !== 0) {
 		estados.push(falta[0]);
@@ -56,31 +57,34 @@ function traduzir() {
 		}
 		falta.shift();
 	}
+
 	for(let i = 0; i < estados.length; i++) {
 		if([...(estados[i])].filter(u => AFN.estadosFinais.includes(u)).length > 0) AFD.estadosFinais.push(i);
 	}
+
 	for(let i = 0; i < temp.length; i++) {
 		AFD.delta[i] = [];
 		for(let j = 0; j < temp[i].length; j++) {
 			AFD.delta[i][j] = estados.indiceconj(temp[i][j]);
 		}
 	}
+
 	AFD.qtdEstados = AFD.delta.length;
 	saida.value = JSON.stringify(AFD, null, "\t");
 }
 
-Set.prototype.igual = function (a) { //Igualdade entre conjuntos
+Set.prototype.igual = function(a) { //Igualdade entre conjuntos
 	if(this.size !== a.size) return false;
 	for(let ele of this) if(!a.has(ele)) return false;
 	return true;
 };
-Array.prototype.contconj = function (a) { //Array contém conjunto
+Array.prototype.contconj = function(a) { //Array contém conjunto
 	for(let ele of this) {
 		if(ele instanceof Set && ele.igual(a)) return true;
 	}
 	return false;
 };
-Array.prototype.indiceconj = function (a) { //Índice de um conjunto em um array
+Array.prototype.indiceconj = function(a) { //Índice de um conjunto em um array
 	for(let i = 0; i < this.length; i++) {
 		if(this[i].igual(a)) return i;
 	}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Tradutor-de-determinismo
-### Unfinished
-### Hosted at https://wcdiaslima.github.io/Tradutor-de-determinismo
+#### "Determinism translator" for finite automata, written in Javascript
+#### Hosted at https://wcdiaslima.github.io/Tradutor-de-determinismo
+###### Developed by Joyce Emanuele and Wellington Cesar
